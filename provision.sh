d58f3adafa81aabd6a83038072b5520540ad2148
Vicente Russo Neto <vicente@codare.com.br>
2015-04-11 00:34:04 -0300

    Vagrant file and provision

---
+++ provision.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+sudo aptitude update -q
+
+# Force a blank root password for mysql
+echo "mysql-server mysql-server/root_password password " | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password " | debconf-set-selections
+
+# Install mysql, nginx, php5-fpm
+apt-get install -q -y -f mysql-server mysql-client nginx php5-fpm
+
+# Install commonly used php packages
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
+
+#Mysql
+for d in "/vagrant/db/"*
+do
+	if [ ! -f "$d/created" ]; then		
+		mysql -uroot <<< "DROP DATABASE IF EXISTS $(basename "$d")"
+		mysql -uroot <<< "CREATE DATABASE $(basename "$d")"
+		if [ -f "$d/dump.sql" ]; then
+			mysql -uroot $(basename "$d") < $d/dump.sql
+		fi
+		sudo touch $d/created
+	fi
+done
+
+#Nginx
+conf="/vagrant/nginx_template/nginx-vhost-template.conf"
+for d in "/vagrant/nginx_template/"*/
+do
+	f=$(basename "$d")
+	if [ ! -f "$d/created" ]; then
+		destconf=/etc/nginx/sites-enabled/$f.conf
+		sudo cp $conf $destconf
+		sudo sed -i -- "s/{{DOMAIN}}/$f/g" $destconf
+		sudo touch $d/created
+	fi
+done
+
+sudo service mysql restart
+sudo service nginx restart
+sudo service php5-fpm restart
