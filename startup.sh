59b7048aaab19ff455a9c0a0077f3de7b45a9681
Vicente Russo <vicente.russo@gmail.com>
2015-07-01 21:37:36 -0300

    Changing default php.ini variables

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+
+sudo sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php5/fpm/php.ini
+sudo sed -i 's/; max_input_vars = .*/max_input_vars = 3000/' /etc/php5/fpm/php.ini
+sudo sed -i 's/max_execution_time = .*/max_execution_time = 300/' /etc/php5/fpm/php.ini
+sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = 64M/' /etc/php5/fpm/php.ini
+
+
 sudo service mysql restart
 sudo service nginx restart
 sudo service php5-fpm restart
